1be11e7de6f05ba97a99fd41365b37ad7a131cbc
riyufuchi <krevelixtv@gmail.com>
2024-12-30 22:28:13 +0100

    Update 1.4
    Improved:
    * Getting information about library and it's license

--- src/main/java/riyufuchi/marvusLib/lib/MarvusLib.java
+++ src/main/java/riyufuchi/marvusLib/lib/MarvusLib.java
@@ -1,12 +1,15 @@
 package riyufuchi.marvusLib.lib;
 
+import riyufuchi.sufuLib.records.SufuLibInfo;
+
 public class MarvusLib
 {
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.4";
+	private static final String licensedTo =  "This version is under general license - /riyufuchi/marvusLib/lib/LICENSE.TXT";
+	public static final SufuLibInfo INFO = new SufuLibInfo("MarvusLib", VERSION, "Riyufuchi", "2022 - 2024", licensedTo, 
+			MarvusLib.class.getResourceAsStream("/riyufuchi/marvusLib/lib/LICENSE.TXT"));
 	
 	public static void main(String[] args)
 	{
 	}
-	
-	// TODO: Add licensing tools
 }
